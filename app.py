63045dfe8184b73aef00e3d73903b3e876135baa
Bastub <bastub57@gmail.com>
2023-03-24 10:07:37 +0100

    404

--- app.py
+++ app.py
@@ -252,7 +252,12 @@ def about():
     cookie = request.cookies.get('theme', default="light")
     return render_template("about.html", loggedin = loggedin(), theme = cookie)
 
+# 404
 
+@app.errorhandler(404)
+def page_not_found(e):
+    cookie = request.cookies.get('theme', default="light")
+    return render_template('404.html', theme = cookie), 404
 
 if __name__ == "__main__" :
     app.run(host="0.0.0.0",port=80,debug=True)
